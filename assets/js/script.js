3252a91742444adb0f8a61a5e221b9186ef4f2d3
dprof-code <olawaleade15@gmail.com>
2025-10-16 18:13:48 +0100

    added source files

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const timeContainer = document.getElementById("current-time");
+
+function updateTime() {
+  const now = new Date();
+  const milliseconds = now.getTime();
+  timeContainer.textContent = milliseconds;
+}
+
+setInterval(updateTime, 1);
+updateTime(); // Initial call to display time immediately
+setInterval(updateTime, 1000);
+updateTime(); // Initial call to display time immediately
